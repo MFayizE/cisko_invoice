0acafcd3e4b2d22d8b6a04cd477dee16142083a3
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-04-19 13:54:30 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,6 +64,10 @@ export class AppComponent  implements OnInit {
       ConsigneeGSTIN: [''],
       ConsigneeState: [''],
       ConsigneeStateCode: [''],
+      state: [''],
+      stateCode: [''],
+
+
 
 
 
